ad3e0f48847e8ebce7dea66d4c0ed9f4b9634c92
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2019-09-20 12:24:07 +0530

    [data] Tile generation from geoTiff

--- tools/data_scripts/vertex_tile_generator.py
+++ tools/data_scripts/vertex_tile_generator.py
@@ -1,11 +1,11 @@
 import numpy as np
 from osgeo import gdal
-# import scipy.misc
+import scipy.misc
 import os
 import csv
 
-SAVE_PATH = "/home/joseph/2-vertex-data/"
-READ_PATH = "/home/joseph/new/"
+SAVE_PATH = "/home/jgeob/quater-ws/satellite-Embedding/geotiff/out/"
+READ_PATH = "/home/jgeob/quater-ws/satellite-Embedding/geotiff/data/"
 
 def pixel2coord(coordsInfo ,x, y):
     """Returns global coordinates from pixel x, y coords"""
@@ -18,7 +18,7 @@ def pixel2coord(coordsInfo ,x, y):
 def tile_logger(filePath, tileID, vertexList,
                 coordsInfo=0, tifName = ""):
     ''' Creates CSV with data on each tile
-    VertexList[0]: Centre,  VertexList[n]: Vn
+    vertexList: list of pixel coord-tuple of vertices
     '''
     latLonList =[]
     if coordsInfo != 0:
@@ -29,22 +29,27 @@ def tile_logger(filePath, tileID, vertexList,
     csvFile.close()
 
 
-### Only Even Tile size Only
+#  Even          Odd
+# ......       .......
+# |  | |       |  |  |
+#    c            c
 def square_tile_gen(array, centre, tileSz=64):
     ''' Return: np.array - square numpy slice with coords as centre
     centre - (x,y) img coords
     '''
     x = centre[0]; y = centre[1]
     sz = tileSz//2
-    res = array[:, (x-sz):(x+sz), (y-sz):(y+sz)].copy()
+    od = tileSz%2
+    res = array[:, (x-sz):(x+sz+od), (y-sz):(y+sz+od)].copy()
     return res
 
-def two_vertix_tiler(array, stride=1, tileSz=64,
-                     savePath = "",filePrefix = "",
+def two_vertex_midpoint_tiler(array, stride=1, tileSz=64,
+                     savePath = "",filePrefix = "tile",
                      coordsInfo = 0, tifName=""):
     '''
     Dumps tiles from 2 points and midpoint tile
     '''
+    join = os.path.join
     C,H,W = array.shape
     sz = tileSz//2
     for i in range(sz, H-sz, tileSz*stride):
@@ -63,16 +68,45 @@ def two_vertix_tiler(array, stride=1, tileSz=64,
                         coordsInfo=coordsInfo, tifName = tifName)
             ####
             #! ! ! CHANGE the `np.astype` to required type
-            np.save(savePath+filePrefix+"-"+str(i)+"-"+str(j)+ "-V1.npy",V1.astype(np.uint8)    , allow_pickle=False)
-            np.save(savePath+filePrefix+"-"+str(i)+"-"+str(j)+ "-V2.npy",V2.astype(np.uint8)    , allow_pickle=False)
-            np.save(savePath+filePrefix+"-"+str(i)+"-"+str(j)+"-Cen.npy",Cen.astype(np.uint8), allow_pickle=False)
-
+            np.save(join(savePath,filePrefix+"-"+str(i)+"-"+str(j)+ "-V1.npy") ,V1.astype(np.uint8)    , allow_pickle=False)
+            np.save(join(savePath,filePrefix+"-"+str(i)+"-"+str(j)+ "-V2.npy") ,V2.astype(np.uint8)    , allow_pickle=False)
+            np.save(join(savePath,filePrefix+"-"+str(i)+"-"+str(j)+"-Cen.npy") ,Cen.astype(np.uint8), allow_pickle=False)
             ###
             ###scipy.misc.imsave(savePath+filePrefix+"-"+str(i)+"-"+str(j)+ "-V1.jpg",(V1[0:3,:,:].transpose(1,2,0)) )
 
 
+def matrix_tiler(array, stride=1, tileSz=50,
+                     savePath = "",filePrefix = "tile",
+                     coordsInfo = 0, tifName=""):
+    '''
+    Dumps tiles of given geoTiff. Naming in matrix type
+    '''
+    join = os.path.join
+    subSavePath = join(savePath, filePrefix)
+    if not os.path.exists(subSavePath):
+        print("** sub SAVE PATH:",subSavePath)
+        os.makedirs(subSavePath)
+
+    C,H,W = array.shape
+    sz = tileSz//2
+    for i in range(sz, H-sz, tileSz*stride):
+        for j in range(sz, W-sz, tileSz*stride):
+
+            L = square_tile_gen(array, (i,j), tileSz)
+
+            tile_logger(savePath, filePrefix+"-"+str(i)+"-"+str(j),
+                        [(i,j)],
+                        coordsInfo=coordsInfo, tifName = tifName)
+            ####
+            #! ! ! CHANGE the `np.astype` to required type
+            np.save(join(subSavePath,filePrefix+"L-"+str(i//tileSz)+"-"+str(j//tileSz)+ ".npy") ,L.astype(np.uint8)    , allow_pickle=False)
+            ###
+            # scipy.misc.imsave(join(subSavePath,filePrefix+"L-"+str(i//tileSz)+"-"+str(j//tileSz)+ ".jpg") ,(L[0:3,:,:].transpose(1,2,0)) )
+
+
+
 for root, dirc, files in os.walk(READ_PATH):
-    savePath = SAVE_PATH+"/"+root.replace(READ_PATH,"")+"/"
+    savePath = os.path.join(SAVE_PATH,root.replace(READ_PATH,"") )
     if not os.path.exists(savePath):
         print("**Creating the Folder for SAVE:",savePath)
         os.makedirs(savePath)
@@ -87,8 +121,8 @@ for root, dirc, files in os.walk(READ_PATH):
         gData = gdal.Open(tifPath)
         geoArray = np.array(gData.ReadAsArray())
         gInfo = gData.GetGeoTransform()
-        two_vertix_tiler(geoArray,
-                        stride = 4,
+        matrix_tiler(geoArray,
+                        stride = 1,
                         savePath = savePath,
                         filePrefix = prefix, # Change PREFIX if Needed
                         coordsInfo = dict(zip(['xoff', 'a', 'b', 'yoff', 'd', 'e'], gInfo)),
