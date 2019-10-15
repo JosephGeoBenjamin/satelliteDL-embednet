1555d96f42abba1f7615190a7a5373307975c4ff
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2019-10-15 13:31:58 +0530

    [tool] GEE snippet

--- tools/GEE_scripts/geoTiff_fetcher-gee.js
+++ tools/GEE_scripts/geoTiff_fetcher-gee.js
@@ -4,8 +4,10 @@
  */
 
 
-var location = {
-  name: "Fresno-T2V",
+var loc = {
+  // USDA - NAIP & NASS_CDL
+  name: "FresnoCDL-T2V",
+  period: ['2016-01-01', '2016-12-31'],
   area: 
     ee.Geometry.Rectangle({
     coords:[[-119.65,37.05],
@@ -14,25 +16,33 @@ var location = {
     })
 };
 
+////////////////// NAIP Images Geotiff ////////////////////////////
+var naip_dataset = ee.ImageCollection('USDA/NAIP/DOQQ')
+                  .filter(ee.Filter.date(loc.period[0], loc.period[1]));
+var img = naip_dataset.filterBounds(loc.area).median();
 
-var dataset = ee.ImageCollection('USDA/NAIP/DOQQ')
-                  .filter(ee.Filter.date('2016-01-01', '2018-12-31'));
+////////////////// NASS - Cropland ////////////////////////////////
+var cdl_dataset = ee.ImageCollection("USDA/NASS/CDL")
+                  .filter(ee.Filter.date(loc.period[0], loc.period[1]));
+var cdl_img = cdl_dataset.filterBounds(loc.area).median();
+
+img = img.addBands(cdl_img)
+///////////////////////////////////////////////////////////////////
 
 
-var img = dataset.filterBounds(location.area).median();
 
-var filepath = "Naip-USA-dumps/cities/" + location.name + '/' + location.name 
+var filepath = "USDA/CDL_NASS_with_NAIP/" + loc.name + '/' + loc.name 
 Export.image.toCloudStorage({
   image: img,
-  description: location.name,
+  description: loc.name,
   bucket: 'satellite-raw-data', 
   fileNamePrefix: filepath,
   scale: 1,
-  region: location.area,
+  region: loc.area,
   fileFormat: 'GeoTIFF',
   maxPixels: 1e13,
   formatOptions: { 
   cloudOptimized: true,
-  fileDimensions: 4096
+  // fileDimensions: 4096
   }
 });
