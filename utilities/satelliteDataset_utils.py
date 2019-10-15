054142a57594f63d74f3818c656ee2e8b94bda88
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2019-10-15 14:12:38 +0530

    [dataLoader] Clubbed data loader

--- utilities/satelliteDataset_utils.py
+++ utilities/satelliteDataset_utils.py
@@ -38,3 +38,43 @@ class PlainTileData(Dataset):
         bands = np.clip(bands, clip_min, clip_max)
         if normalize: bands = bands / (clip_max - clip_min)
         return bands
+
+
+class ClubedTileData(Dataset):
+    """ Loads all tiles in npy format in folder recursively
+    """
+    def __init__ (self, dataPath, dataCSV=None, img_ext=".npy"):
+        if not os.path.exists(dataPath): sys.exit("Enter a Valid READ_PATH")
+
+        self.img_ext = img_ext
+        self.club_sz = len(dataPath)
+        self.filePathList = []
+
+        if not dataCSV:
+            for fth in range(self.club_sz)
+                tempList = glob.glob( dataPath[fth]+"/**/*"+img_ext ,recursive=True)
+                self.filePathList.append(sorted(tempList))
+                print("Loaded files from:" dataPath[fth])
+        else :
+            sys.exit("Clubed data loading Not implemented for CSV")
+
+        for path in self.filePathList:
+            if not ( len(filePathList[0]) == len(path) ):
+                sys.exit("MisMatch in the Loaded file counts of Clubbing")
+
+    def __getitem__(self, idx):
+        img = []
+        for fth in range(self.club_sz)
+            tempImg = np.load(self.filePathList[fth][idx])
+            tempImg = self.clip_scale_bands(tempImg)
+            tempImg = torch.from_numpy(tempImg).type(torch.FloatTensor)
+            img.append(Variable(tempImg))
+        return img
+
+    def __len__(self):
+        return len(self.filePathList[0])
+
+    def clip_scale_bands(self, bands, normalize=True, clip_min=0, clip_max=255):
+        bands = np.clip(bands, clip_min, clip_max)
+        if normalize: bands = bands / (clip_max - clip_min)
+        return bands
